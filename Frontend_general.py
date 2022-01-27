c88a0ed6b89b6286f179abbd5c40926bc974cd65
Brake <73952103+VixNam21@users.noreply.github.com>
2022-01-27 11:39:22 +0530

    Update Frontend_general.py

--- Frontend_general.py
+++ Frontend_general.py
@@ -1,51 +1,38 @@
 from tkinter import *
 from tkinter.ttk import *
-from PIL import ImageTk,Image
 import requests
 from bs4 import BeautifulSoup
+import webbrowser
+
 
 html_text = requests.get('https://timesofindia.indiatimes.com/world').text
 soup = BeautifulSoup(html_text, 'html.parser')
 head = soup.find('div', class_ = 'top-newslist')
 
 titles = []
-links = []
 
 for title in head.find_all('a'):
     headline = title.get('title')
     titles.append(headline)
 
-for link in head.find_all('a'):
-    a = 'https://timesofindia.indiatimes.com'+link.get('href')
-    links.append(a)
 
-length = len(titles)
-
-
-with open("news.txt",'w') as f:
-    for i in range (len(titles)-1):
-        n = str(i + 1)+')'
-        f.write(n)
-        f.write('TITLE:'+titles[i+1]+'\n\r')
-        f.write(' '+'LINK:'+links[i+1]+'\n\r')
-        f.write('\n\r')
-
-#need you to do you thing here
-def output():
-    with open("news.text",'r') as f:
-        list = f.readlines()
-        text = list[-1]
-        return text
-
-news=output()
 
 root = Tk()
 root.geometry('600x700')
 root.title('COVID-Care')
 root.configure(background='#5d8a82')
 
-
-
-label= Label(root,text="news",width=600).place(relx=0.5,rely=0.5,anchor=CENTER)
+my_listbox = Listbox(root, width = 55, height = 60, font = ('Times', 18))
+my_listbox.pack(pady = 100)
+i = 0
+j = 1
+for news in titles[1::]:
+    if i < 3*len(titles) - 1:
+        my_listbox.insert(i, news)
+        i = i + 1
+        my_listbox.insert(i, '\n\n\n\n')
+        i = i + 1
 
 root.mainloop()
+
+print(titles)
