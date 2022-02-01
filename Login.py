f884c3efcd3eeb04ef9ac1aee3224bac6cc92a02
Snapman5678 <89893519+Snapman5678@users.noreply.github.com>
2022-02-01 09:13:03 +0530

    Update Login.py
    new button for statenews

--- Login.py
+++ Login.py
@@ -15,6 +15,10 @@ def mainnext_general():
     root.destroy()
     import Frontend_general
 
+def mainnext_statewise():
+    root.destroy()
+    import statewisefinal
+
 
 progress = Progressbar(root, orient=HORIZONTAL, length=600, mode='determinate', )
 
@@ -42,10 +46,23 @@ def bar2():
     progress['value'] = 100
     mainnext_general()
 
+def bar3():
+    import time
+    progress['value'] = 20
+    root.update_idletasks()
+    time.sleep(1)
+
+    progress['value'] = 80
+    root.update_idletasks()
+    time.sleep(1)
+    progress['value'] = 100
+    mainnext_statewise()
+
 progress.place(relx=0.5, rely=0.98, anchor=CENTER)
 
-button_covid = Button(root,text="Covid News",command=bar1).place(relx=0.3,rely=0.5,anchor=CENTER)
-button_gen = Button(root,text="General News",command=bar2).place(relx=0.6,rely=0.5,anchor=CENTER)
+button_covid = Button(root,text="Covid News",command=bar1).place(relx=0.5,rely=0.3,anchor=CENTER)
+button_gen = Button(root,text="General News",command=bar2).place(relx=0.5,rely=0.5,anchor=CENTER)
+button_state = Button(root,text="Statewise News",command=bar3).place(relx=0.5,rely=0.7,anchor=CENTER)
 
 
 root.mainloop()
