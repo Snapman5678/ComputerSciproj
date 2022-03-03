33f272574247cd1486430db122791168951e51d5
Brake <73952103+VixNam21@users.noreply.github.com>
2022-03-03 20:22:33 +0530

    Update Profile.py

--- Profile.py
+++ Profile.py
@@ -3,6 +3,8 @@ from tkinter.ttk import *
 from tkinter import *
 import mysql.connector as sqltor
 import os 
+import matplotlib.pyplot as plt
+
 
 root = tk.Tk()
 root.geometry('600x700')
@@ -123,6 +125,37 @@ def subboo():
     boolabel.config(text = boosterpro)
     mycon.close()
     return boosterpro
+#edit PROFILE
+
+def editprofile():
+
+    import EditProfile
+#Graph Thing
+
+def graphstats():
+    passwordsql = os.environ.get('SQL_PASS')
+
+    mycon = sqltor.connect(host = 'localhost', user = 'root', passwd = passwordsql, database ='projectc12')
+
+    cursor = mycon.cursor()
+
+    st = 'SELECT VACCINE, BOOSTERS FROM USERS;'
+
+    cursor.execute(st)
+
+    res = cursor.fetchall()
+
+    vac = []
+    boos = []
+
+    for i in res:
+        print(i[0], i[1])
+        vac.append(i[0])
+        boos.append(i[1])
+    
+    plt.scatter(boos, vac)
+    plt.show()
+
 
 #DISPLAYING EVERYTHING 
 
@@ -133,21 +166,23 @@ namelbl.place(x = 350, y = 50)
 
 vaccdisp = tk.Label(root, text = 'VACCINES TAKEN:', fg ='black', bg = 'white', font = ('Helvetica', 25))
 vaccdisp.place(x = 50, y = 100)
-vacbutton2 = tk.Button(root, command = subvac, text = "-", width = 1)
-vacbutton2.place(x = 340, y = 100)
 vacclabel = tk.Label(root, text = vaccinepro , bg = 'white', fg ='black', font = ('Helvetica', 25))
 vacclabel.place(x = 390, y = 100)
-vaccbutton = tk.Button(root, command = addvac, text = "+", width = 1)
-vaccbutton.place(x = 440, y = 100)
+
 
 
 boodisp = tk.Label(root, text = "BOOSTERS TAKEN:", fg = 'black', bg = 'white', font = ('Helvetica', 25))
 boodisp.place(x = 50, y = 150)
 boolabel = tk.Label(root, text = boosterpro , bg = 'white', fg ='black', font = ('Helvetica', 25))
 boolabel.place(x = 390, y = 150)
-boobutton = tk.Button(root, text = "-", command = subboo, width = 1)
-boobutton.place(x = 340, y = 150)
-boobutton2 = tk.Button(root, text = "+", command = addboo, width = 1)
-boobutton2.place(x = 440, y = 150)
 
+
+graphbutton = tk.Button(root, text = 'SHOW STATISTICS', command = graphstats, font = ('Helvetica', 20))
+graphbutton.place(x = 50, y = 300)
+
+editpro = tk.Button(root, text = 'EDIT PROFILE', command = editprofile, font = ('Helvetica', 25))
+editpro.place(x = 50, y = 400)
 root.mainloop()
+
+
+
